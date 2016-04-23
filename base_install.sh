c31ce1040b29ef4781b172c2a712ad318efee4a9
Colin Shea <colin@evaryont.me>
2016-04-23 23:31:07 +0000

    configure the salt-master in the script, too

--- base_install.sh
+++ base_install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# simple check if the machine's hostname matches the first argument
 hostnameMatches() {
   hostname | grep $1 > /dev/null
   return $?
 }
 
+# Enable a service in systemd
 enableService() {
   shortName=$(basename $1)
 
@@ -25,12 +27,29 @@ pacman -Syu --noconfirm
 echo "Installing salt on Arch"
 pacman -S salt-zmq --noconfirm
 
-enableService salt-minion
+# Check if we're the salt master, and do some basic configuration
 if hostnameMatches master; then
+  cat >/etc/salt/master <<EOF
+file_roots:
+  base:
+    - /srv/salt
+
+pillar_roots:
+  base:
+    - /srv/pillar
+
+auto_accept: True
+hash_type: sha256
+EOF
+
   enableService salt-master
 fi
 
+# Do some more basic configuration for the salt minion for everyone
 cat >/etc/salt/minion <<EOF
 # vagrant script base_install.sh put this here
 master: 10.20.30.11
+hash_type: sha256
 EOF
+
+enableService salt-minion
